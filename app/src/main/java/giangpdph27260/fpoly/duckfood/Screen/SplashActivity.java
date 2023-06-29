955b44f4a8cab0ca968bbc94160573ef60dce155
giangpham <giangpdph27260@fpt.edu.vn>
2023-06-29 22:13:53 +0700

    fix loi

--- app/src/main/java/giangpdph27260/fpoly/duckfood/Screen/SplashActivity.java
+++ app/src/main/java/giangpdph27260/fpoly/duckfood/Screen/SplashActivity.java
@@ -1,8 +1,8 @@
-package giangpdph27260.fpoly.duckfood.Activity;
+package giangpdph27260.fpoly.duckfood.Screen;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -12,24 +12,23 @@ import androidx.core.content.ContextCompat;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
-import java.time.Instant;
-
 import giangpdph27260.fpoly.duckfood.R;
 
-public class SlashActivity extends AppCompatActivity {
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Window window = getWindow();
-        window.setStatusBarColor(ContextCompat.getColor(this, R.color.trangchu));
-        setContentView(R.layout.activity_slash);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.home));
+        setContentView(R.layout.activity_splash);
 
-        ImageView logoDuckfood = findViewById(R.id.logo_duckfood);
+        ImageView logoDuckFood = findViewById(R.id.logo_duckfood);
         Glide.with(this)
                 .load(R.drawable.logo_duckfood)
                 .apply(RequestOptions.circleCropTransform())
-                .into(logoDuckfood);
+                .into(logoDuckFood);
 
 
     }
