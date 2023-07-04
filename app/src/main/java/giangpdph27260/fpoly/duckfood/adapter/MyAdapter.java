bbece8dd7d59f7ebd4b6fa4809179e9a4b222ebb
DESKTOP-PSBQ5KQ\admin <ducpt@ftech.ai>
2023-07-04 11:44:52 +0700

    refactor code scraping data

--- app/src/main/java/giangpdph27260/fpoly/duckfood/adapter/MyAdapter.java
+++ app/src/main/java/giangpdph27260/fpoly/duckfood/adapter/MyAdapter.java
@@ -1,5 +1,6 @@
 package giangpdph27260.fpoly.duckfood.adapter;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +13,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import giangpdph27260.fpoly.duckfood.R;
 import giangpdph27260.fpoly.duckfood.modal.Category;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
-    private final ArrayList<Category> listCategory;
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private ArrayList<Category> listCategory = new ArrayList<>();
 
-    public MyAdapter(ArrayList<Category> list) {
-        this.listCategory = list;
+    @SuppressLint("NotifyDataSetChanged")
+    public void setListCategory(List<Category> categories) {
+        this.listCategory = new ArrayList<>(categories);
+        notifyDataSetChanged();
     }
+
     @NonNull
     @Override
     public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -32,13 +37,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
-
-        Category category = listCategory.get(position);
-
-        Glide.with(holder.itemView)
-                .load(category.getImageUrl())
-                .into(holder.imgItemCategory);
-        holder.tvItemTitle.setText(category.getTitle());
+        holder.onBind(position);
     }
 
     @Override
@@ -46,16 +45,26 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
         return listCategory.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         private final ImageView imgItemCategory;
         private final TextView tvItemTitle;
 
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            itemView.setOnClickListener(view -> {
+                //TODO xử lý click item
+            });
             imgItemCategory = itemView.findViewById(R.id.img_item_category);
             tvItemTitle = itemView.findViewById(R.id.tv_item_title);
+        }
 
+        public void onBind(int position) {
+            Category category = listCategory.get(position);
+            Glide.with(itemView)
+                    .load(category.getImageUrl())
+                    .into(imgItemCategory);
+            tvItemTitle.setText(category.getTitle());
         }
     }
 }
--- app/src/main/java/giangpdph27260/fpoly/duckfood/modal/Category.java
+++ app/src/main/java/giangpdph27260/fpoly/duckfood/modal/Category.java
@@ -1,10 +1,13 @@
 package giangpdph27260.fpoly.duckfood.modal;
 
+import androidx.annotation.NonNull;
+
 public class Category {
     private String title;
     private String imageUrl;
 
-    public Category(){}
+    public Category() {
+    }
 
     public String getTitle() {
         return title;
@@ -20,5 +23,11 @@ public class Category {
 
     public void setImageUrl(String image) {
         this.imageUrl = image;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Catefory: {title: " + title + ", imageUrl: " + imageUrl + "}";
     }
 }
